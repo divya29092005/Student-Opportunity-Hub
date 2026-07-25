085087300546327ec072f356a04368eca675e3ce
Divya Dharshini <dharshinideena2005@gmail.com>
2026-07-25 09:32:07 +0530

    Fixed routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import Opportunities from "./pages/Opportunities";
-import Details from "./pages/Details";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/opportunities" element={<Opportunities />} />
-      <Route path="/details" element={<Details />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
